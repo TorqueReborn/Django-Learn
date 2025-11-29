0ea4cf4ad2ba198d49d714161a8283f769c5a677
TorqueReborn <torquereborn@gmail.com>
2025-11-29 17:15:34 +0530

    Disable safe to render json

--- snippets/views.py
+++ snippets/views.py
@@ -12,7 +12,7 @@ def snippet_list(request):
     if request.method == "GET":
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, safe=True)
+        return JsonResponse(serializer.data, safe=False)
     elif request.method == "POST":
         data = JSONParser().parse(request)
         serializer = SnippetSerializer(data=data)
